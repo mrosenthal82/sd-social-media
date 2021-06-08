8bd10dc5967353b29512a3ec33ad220e27b2ccc7
Meredith Rosenthal <mrosenthal@ucvts.org>
2021-06-08 13:42:51 -0400

    ViewManager initial setup

---
+++ src/controller/ViewManager.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.awt.CardLayout;
+import java.awt.Container;
+
+import model.User;
+
+public class ViewManager {
+	
+	private static ViewManager manager;
+	private Container views;
+	
+	private ViewManager(Container views) {
+        this.views = views;
+    }
+	
+	public static ViewManager getInstance(Container views) {
+        if (manager == null) {
+            manager = new ViewManager(views);
+        }
+        return manager;
+    }
+	
+	public void switchTo(String view) {
+        ((CardLayout) views.getLayout()).show(views, view);
+    }
+}
